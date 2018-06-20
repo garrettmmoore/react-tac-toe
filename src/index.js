051a5490894e36a1f19faddc200b167e435a3034
Garrett Moore <garrettmarkmoore@gmail.com>
2018-06-19 18:23:38 -0700

    Add constructor to Board component & set initial state to contain an array w/ 9 nulls

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ class Square extends React.Component {
       value: null,
     };
   }
+
   render() {
     return (
       <button className="square" onClick={() => this.setState({ value: 'X' })}>
@@ -23,6 +24,13 @@ class Square extends React.Component {
 
 // Board component
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
     return <Square value={i} />;
   }
