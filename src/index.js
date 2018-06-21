545667b9c4cb6e0a4bfe992de562db2b10c192f0
Garrett Moore <garrettmarkmoore@gmail.com>
2018-06-20 18:46:35 -0700

    Update Game's handleClick to be aware of stepNumber when reading the current board state

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -72,6 +72,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
